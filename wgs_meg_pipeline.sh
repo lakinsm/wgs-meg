36cd5bb15be6f73cc1cf220940f1eeda13e8e2f4
Steven Lakin <lakinsm@rams.colostate.edu>
2016-04-28 16:32:39 -0600

    Barebones implementation complete, kSNP framework outlined but not yet implemented.  We need to decide how to handle continual update of phylo tree.

--- wgs_meg_pipeline.sh
+++ wgs_meg_pipeline.sh
@@ -615,9 +615,10 @@ validate_inputs $forward $reverse
 ##############
 ## Assembly ##
 ##############
-echo -e "Beginning Assembly pipeline..."
-echo -e "Beginning Assembly pipeline..." >> WGS_LabNotebook.txt
 if [ "${run_assembly}" == "1" ]; then
+    echo -e "Beginning Assembly pipeline..."
+    echo -e "Beginning Assembly pipeline..." >> WGS_LabNotebook.txt
+    
     ## Find average insert size with BBMerge from BBMap
     bbmap_insert_size $forward $reverse
 
